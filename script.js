6cb92a2d018465e53354df2bee04badf3f17973a
naveen kumar <157386680+naveenkumarkannan2004@users.noreply.github.com>
2024-02-19 21:52:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,27 +2,33 @@ function findMetroLine() {
     var startStation = document.getElementById("startStation").value;
     var stopStation = document.getElementById("stopStation").value;
     var resultDiv = document.getElementById("result");
-
-    // Perform logic to determine the metro line based on start and stop stations
     var metroLine = getMetroLine(startStation, stopStation);
-
-    // Display the result
     resultDiv.innerHTML = "Take Metro Line: " + metroLine;
 }
 
 function getMetroLine(startStation, stopStation) {
-    let blue=['lic','tenabet','airport'];
-    let green=['shenoy nagar','koyambedu','kilpauk'];
-    
-    if (startStation in blue && stopStation in blue) {
-        return "Blue Line";
-    } else if (startStation in green && stopStation in green) {
-        return "Red Line";
+    let green=["Anna Nagar East","Anna Nagar Tower","Arumbakkam","Ashok Nagar",
+"Egmore","Ekkattuthangal","Kilpauk","Koyambedu","Nehru Park","Pachaiyappa's College",
+"Puratchi Thalaivi Dr. J. Jayalalithaa CMBT","Shenoy Nagar","St. Thomas Mount",
+"Thirumangalam","Vadapalani"];
+    let blue=["AG – DMS","Chennai International Airport","Government Estate",
+"Guindy","High Court","Kaladipet","LIC","Little Mount","Mannadi","Meenambakkam",
+"Nandanam","Nanganallur Road","New Washermanpet","Saidapet","Sir Theagaraya College",
+"Teynampet","Tiruvottriyur","Tiruvottriyur Theradi","Thousand Lights","Tollgate","Tondiarpet",
+"Washermanpet","Wimco Nagar"];
+    if (green.includes(startStation) && green.includes(stopStation)) {
+        return "green Line";
+    } else if (blue.includes(startStation) && blue.includes(stopStation)) {
+        return "blue Line";
     }
-    else if(startStation in green && stopStation in blue)||(startStation in blue && stopStation in green){
+    else if (blue.includes(startStation) && green.includes(stopStation)){
         return "use station central or alandur";
-    }
-    else {
+}
+else if (green.includes(startStation) && blue.includes(stopStation)){
+        return "use station central or alandur";
+}
+ else {
         return "Line not found";
     }
 }
+
